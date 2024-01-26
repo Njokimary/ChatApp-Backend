79d9af0266c386bd341401fefd415eae561cafdf
Njokimary <marynjoki738@gmail.com>
2024-01-26 16:10:14 +0300

    feat:initialize database

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 db = SQLAlchemy(app)
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
-    timestamp = db.Column(db.String(20))
-    message_body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime)
+    body = db.Column(db.String)
 
-# Create database tables
-db.create_all()
+# Define a function to create the app context and initialize the database
+def create_app():
+    with app.app_context():
+        db.create_all()
+        return app
 
-    
+
+
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+    create_app().run(debug=True)
