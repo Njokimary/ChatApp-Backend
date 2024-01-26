e39b33d6c57c6d0baa7c8347548e1b4335551137
Njokimary <marynjoki738@gmail.com>
2024-01-26 13:13:50 +0300

    feat:create the message model

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 db = SQLAlchemy(app)
 
-
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    timestamp = db.Column(db.String(20))
+    message_body = db.Column(db.Text)
+    
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
