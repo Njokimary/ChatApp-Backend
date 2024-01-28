cc7e0432be9ed4aeeb96ed1a45d8507e7cfb980e
Njokimary <marynjoki738@gmail.com>
2024-01-28 22:02:29 +0300

    feat:transfer csv data to database

--- seed.py
+++ seed.py
@@ -1,32 +1,27 @@
+# seed.py
 import pandas as pd
-from flask_seeder import Seeder
-from app import create_app, db, Message  
+from app import create_app, db, Message
 from datetime import datetime
 
-class MessageSeeder(Seeder):
+class MessageSeeder:
     def run(self):
-        # Read data from the CSV file using pandas
-        data = pd.read_csv('/home/njoki/Downloads/GeneralistRails_Project_MessageData.csv')
+        app = create_app()
 
-        # Iterate over the rows and insert data into the database
-        for index, row in data.iterrows():
-            timestamp = datetime.strptime(row['Timestamp (UTC)'], '%Y-%m-%d %H:%M:%S')
-            message = Message(
-                user_id=row['User ID'],
-                timestamp=timestamp,
-                body=row['Message Body']
-            )
-            db.session.add(message)
+        with app.app_context():
+            # Read data from the CSV file using pandas
+            data = pd.read_csv('/home/njoki/Downloads/GeneralistRails_Project_MessageData.csv')
 
-       
-        db.session.commit()
+            # Iterate over the rows and insert data into the database
+            for index, row in data.iterrows():
+                timestamp = datetime.strptime(row['Timestamp (UTC)'], '%Y-%m-%d %H:%M:%S')
+                message = Message(
+                    user_id=row['User ID'],
+                    timestamp=timestamp,
+                    body=row['Message Body']
+                )
+                db.session.add(message)
 
-# Create application context
-app = create_app()
+            db.session.commit()
 
-# Run the seeder within the application context
-with app.app_context():
-   
-    db.create_all()
-
+if __name__ == "__main__":
     MessageSeeder().run()
